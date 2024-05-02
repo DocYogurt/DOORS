8739ae5a6393d31eebbc67031c735d39cac87b3b
DocYogurt <58424831+DocYogurt@users.noreply.github.com>
2024-05-02 16:51:26 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,12 @@
+lcoal GL = {
+    [{6839171747}] = "NBDOORS"
+}
+
+for GI, U in next, GL do
+    if table.find(GI, game.PlaceId) then
+        loadstring(game:HttpGet("https://github.com/DocYogurt/DOORS/raw/main/Scripts/Gui/"..U))()
+        return
+    else
+        loadstring(game:HttpGet("https://github.com/DocYogurt/DOORS/raw/main/Scripts/Gui/ItemHub"))()
+    end
+end
